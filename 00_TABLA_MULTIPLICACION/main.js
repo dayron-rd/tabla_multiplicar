5b267edaf364771bbc734361b600e256c0066586
dayron-rd <ing.dayron.rd@gmail.com>
2025-02-10 09:31:09 +0100

    se crean los apuntadores en JS al HTML

---
+++ 00_TABLA_MULTIPLICACION/main.js
@@ -0,0 +1,4 @@
+let numero_tabla = document.getElementById(numero_tabla);
+let etiqueta = document.getElementById(texto_help);
+let tabla_lista = document.getElementById(lista);
+const boton = document.getElementById(generar);
