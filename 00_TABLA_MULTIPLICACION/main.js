b0595f2b435076d1f17383b2cb28bf4342ab9fe6
dayron-rd <ing.dayron.rd@gmail.com>
2025-02-10 09:53:46 +0100

    JS chequea que el input sea un numero del 1-9

--- 00_TABLA_MULTIPLICACION/main.js
+++ 00_TABLA_MULTIPLICACION/main.js
@@ -1,4 +1,27 @@
-let numero_tabla = document.getElementById(numero_tabla);
-let etiqueta = document.getElementById(texto_help);
-let tabla_lista = document.getElementById(lista);
-const boton = document.getElementById(generar);
+let numero = document.getElementById('numero_tabla')
+let etiqueta = document.getElementById('texto_help')
+let tabla_lista = document.getElementById('lista')
+let boton = document.getElementById('generar')
+let check_input = false
+let resultado 
+
+
+
+function calcular(){
+    check();
+
+}
+
+function check(){
+    const valor = numero.value;
+    console.log(valor);
+    if(valor != undefined && (valor>=1 && valor <=9)){
+        check_input = true
+        etiqueta.innerText = ""
+    }else{
+        etiqueta.innerText = "Ingrese un # del 1 al 9"
+    }
+}
+
+
+boton.addEventListener("click", check);
