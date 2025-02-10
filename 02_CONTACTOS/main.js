343208d340622ba74c002bba71180724b726f85d
dayron-rd <ing.dayron.rd@gmail.com>
2025-02-10 13:45:51 +0100

    Se declaran los punteros a los elementos de la sección izquierda del HTML en el JS

---
+++ 02_CONTACTOS/main.js
@@ -0,0 +1,4 @@
+const nombre = document.getElementById("name");
+const telefono = document-getElementById("phone");
+const correo = document.getElementById("email");
+const botonAgregar = document.getElementById("add");
