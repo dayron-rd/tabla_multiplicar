a654ffd4f32d07325e3130f12b85ada5179b3192
dayron-rd <ing.dayron.rd@gmail.com>
2025-02-10 10:10:19 +0100

    se crea el for para generar la tabla y se imprime el listado en el html

--- 00_TABLA_MULTIPLICACION/main.js
+++ 00_TABLA_MULTIPLICACION/main.js
@@ -9,12 +9,20 @@ let resultado
 
 function calcular(){
     check();
-
+    tabla_lista.innerHTML = ""
+    if (check_input){
+        for(let i = 1; i <= 10; i++){
+            resultado = i * numero.value
+            const node = document.createElement("li");
+            const textnode = document.createTextNode(i + " * " + numero.value + " = " + resultado);
+            node.appendChild(textnode);
+            document.getElementById("lista").appendChild(node);
+        }
+    }
 }
 
 function check(){
     const valor = numero.value;
-    console.log(valor);
     if(valor != undefined && (valor>=1 && valor <=9)){
         check_input = true
         etiqueta.innerText = ""
@@ -24,4 +32,4 @@ function check(){
 }
 
 
-boton.addEventListener("click", check);
+boton.addEventListener("click", calcular);
