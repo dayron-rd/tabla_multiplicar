5a2fdd1516829268266aa9f01a10b368fa859897
dayron-rd <ing.dayron.rd@gmail.com>
2025-02-10 19:17:33 +0100

    Funcionamiento completo, se agregan los elementos con <div> según se presiona el botón agregar, dentro de cada <div> se le asigna una clase CSS para modificar su apariencia y posteriormente se agrega la información a mostrar en cada uno

--- 02_CONTACTOS/main.js
+++ 02_CONTACTOS/main.js
@@ -3,14 +3,63 @@
 const sectionLibreta = document.getElementById('libreta')
 
 /* Obtener elementos del formulario */
-const nombre = document.getElementById('name');
-const telefono = document.getElementById('phone');
-const correo = document.getElementById('email'); 
+let nombre = document.getElementById('name');
+let telefono = document.getElementById('phone');
+let correo = document.getElementById('email'); 
 const botonAgregar = document.getElementById('add');
 
 
 function agregarContacto(){
-    console.log(nombre.value + ' ' + telefono.value + ' ' + correo.value);
+    // Toma los valores de los input en el momento de la interrupción
+    const getNombre = nombre.value;
+    const getTelefono = telefono.value;
+    const getCorreo = correo.value;
+
+    // Chequea que los tres campos estén rellenos
+    if(getNombre && getTelefono && getCorreo){
+        /* Crea un contenedor para el contacto */
+        const contacto = document.createElement('div');
+        contacto.classList.add('contact');
+
+        /* Crea la inicial */
+        const inicial = document.createElement('div');
+        inicial.classList.add('icon');
+        inicial.textContent = getNombre.charAt(0).toUpperCase();
+
+        /* Crear la información */
+        const información = document.createElement('div')
+        información.classList.add('data_list')
+
+        const nombreElemento = document.createElement('h3');
+        nombreElemento.innerHTML = getNombre;
+
+        const telefonoElemento= document.createElement('p');
+        información.classList.add('p_list_phone')
+        telefonoElemento.innerHTML = '📱 '+getTelefono;
+
+        const emailElemento = document.createElement('p');
+        información.classList.add('p_list_email')
+        emailElemento.innerHTML = '📨 ' + getCorreo;
+
+        /* Agregar los elementos a la información */
+        información.appendChild(nombreElemento);
+        información.appendChild(telefonoElemento);
+        información.appendChild(emailElemento);
+
+        /* Agregar la inicial y la información al contacto */
+        contacto.appendChild(inicial);
+        contacto.appendChild(información);
+
+        /* Agregar el contacto a la lista */
+        sectionLibreta.appendChild(contacto);
+
+        /* limpiar los campos del formulario */
+        nombre.value = "";
+        telefono.value = "";
+        correo.value = "";
+    }else{
+        alert('Por favor, rellene los tres campos')
+    }
 
 }
 
