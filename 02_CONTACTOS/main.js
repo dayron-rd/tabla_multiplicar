0cc9d45c6a8b13b6b5f571c72d0a389b1fcf5238
dayron-rd <ing.dayron.rd@gmail.com>
2025-02-10 18:00:24 +0100

    Se modifica el html para dejarlo listo para la creación de los elementos en la sección de la derecha, el JS capta el nombre, telefono y email de la sección de la derecha al presionar el boton

--- 02_CONTACTOS/main.js
+++ 02_CONTACTOS/main.js
@@ -1,4 +1,17 @@
-const nombre = document.getElementById("name");
-const telefono = document-getElementById("phone");
-const correo = document.getElementById("email");
-const botonAgregar = document.getElementById("add");
+/* Referencia a los elementos del DOM */
+/* const sectionDatos = document.getElementById('datos') */
+const sectionLibreta = document.getElementById('libreta')
+
+/* Obtener elementos del formulario */
+const nombre = document.getElementById('name');
+const telefono = document.getElementById('phone');
+const correo = document.getElementById('email'); 
+const botonAgregar = document.getElementById('add');
+
+
+function agregarContacto(){
+    console.log(nombre.value + ' ' + telefono.value + ' ' + correo.value);
+
+}
+
+botonAgregar.addEventListener('click', agregarContacto);
