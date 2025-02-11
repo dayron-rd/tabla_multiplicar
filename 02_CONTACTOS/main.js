48b5393f66d756f42ff8d9737432363058596907
dayron-rd <ing.dayron.rd@gmail.com>
2025-02-11 08:33:00 +0100

    commit antes de traer el resultado final

--- 02_CONTACTOS/main.js
+++ 02_CONTACTOS/main.js
@@ -2,3 +2,4 @@ const nombre = document.getElementById("name");
 const telefono = document-getElementById("phone");
 const correo = document.getElementById("email");
 const botonAgregar = document.getElementById("add");
+
